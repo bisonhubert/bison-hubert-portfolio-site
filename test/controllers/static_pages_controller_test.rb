576ed4639059ddfdef1390524d3fc5f1c0a32dcd
Bison Hubert <davidbisonhubert@gmail.com>
2017-05-22 22:58:08 -0700

    Add dynamic titles to pages and related tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Bison Hubert"
+  end
+
   test "should get home" do
     get root_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get portfolio" do
     get projects_url
     assert_response :success
+    assert_select "title", "Portfolio | #{@base_title}"
   end
 
 end
