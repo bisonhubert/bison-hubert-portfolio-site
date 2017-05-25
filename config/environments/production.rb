b7a54d6db1aeaf290f7107c9ad891d705ec639cf
Bison Hubert <davidbisonhubert@gmail.com>
2017-05-25 12:33:15 -0700

    Don't fall back to assets pipeline if precompiled asset is missed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.precompile =  ['*.js', '*.css', '*.css.erb', '*.scss'] 
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
