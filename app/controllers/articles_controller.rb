835e023a9be796c0a92159f71faab0ce6566f295
Bison Hubert <davidbisonhubert@gmail.com>
2017-06-05 16:56:07 -0700

    Add update action and pull form into partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,12 +3,21 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
+    
     if @article.save
       redirect_to @article
     else
@@ -16,12 +25,18 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
+  def update
     @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   private
     def article_params
-      params.require(:article).permit(:title, :text)
+      params.require(:article).permit(:title, :text, :image, :summary)
     end
 end
