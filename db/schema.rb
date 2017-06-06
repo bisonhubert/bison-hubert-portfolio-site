e6af3676be610485dc974af5c31f59c1d206e8a2
Bison Hubert <davidbisonhubert@gmail.com>
2017-06-05 17:59:04 -0700

    Add user validations and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602234517) do
+ActiveRecord::Schema.define(version: 20170606005618) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20170602234517) do
     t.string   "image"
     t.string   "demo_link"
     t.string   "github_link"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
