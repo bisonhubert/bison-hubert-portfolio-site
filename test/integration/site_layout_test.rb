77096bba41be4708c85dfe82254f9d03f6f90dd7
Bison Hubert <davidbisonhubert@gmail.com>
2017-06-05 17:49:57 -0700

    Update site nav for home path to use root_path

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,16 +12,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/under_construction'
     assert_select "h1", "Bison Hubert Portfolio Site"
 
-    get blog_path
-    assert_template 'articles/index'
-    assert_select "a[href=?]", portfolio_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", home_path
-
     get about_path
     assert_template 'static_pages/about'
     assert_select "a[href=?]", portfolio_path
     assert_select "a[href=?]", blog_path
-    assert_select "a[href=?]", home_path
+    assert_select "a[href=?]", root_path
   end
 end
