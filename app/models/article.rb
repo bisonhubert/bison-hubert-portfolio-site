8f56336ff73d21d1319a054f559cb16701a0abc3
Bison Hubert <davidbisonhubert@gmail.com>
2017-06-05 17:49:36 -0700

    Add validations to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  validates :title,   presence: true,
+                      length: { minimum: 5 }
+  validates :text,    presence: true
+  validates :image,   presence: true
+  validates :summary, presence: true,
+                      length: { maximum: 140 }
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @article = Article.new(title: 'Test Article', text: 'Sample text', summary: 'This is a test article', image: 'fakeimage.jpg')
+  end
+
+  test 'should be valid' do
+    assert @article.valid?
+  end
+
+  test 'title should be present' do
+    @article.title = ''
+    assert_not @article.valid?
+  end
+
+  test 'title should not be too short' do
+    @article.title = '%' * 4
+    assert_not @article.valid?
+  end
+
+  test 'text should be present' do
+    @article.text = ''
+    assert_not @article.valid?
+  end
+
+  test 'summary should be present' do
+    @article.summary = ''
+    assert_not @article.valid?
+  end
+
+  test 'summary should not be too long' do
+    @article.summary = '%' * 141
+    assert_not @article.valid?
+  end
+
+  test 'image should be present' do
+    @article.image = ''
+    assert_not @article.valid?
+  end
 end
