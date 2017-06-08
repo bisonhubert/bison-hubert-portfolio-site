f0deefb963d3281decdfe83a57fe9475f5835d89
Bison Hubert <davidbisonhubert@gmail.com>
2017-06-07 21:11:03 -0700

    Add blog, redirect links to under construction page, and remove unused tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get '/home',        to: 'static_pages#home'
-  get '/about',       to: 'static_pages#about'
-  get '/portfolio',   to: 'static_pages#under_construction'
-  get '/resume',      to: 'static_pages#resume'
-  get '/blog',        to: 'articles#index'
+  get '/home',                to: 'static_pages#home'
+  get '/about',               to: 'static_pages#about'
+  get '/portfolio',           to: 'static_pages#portfolio'
+  get '/resume',              to: 'static_pages#resume'
+  get '/blog',                to: 'articles#index'
+  get '/under-construction',  to: 'static_pages#under_construction'
 
   resources :users
   resources :articles
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,18 +4,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", portfolio_path
-    assert_select "a[href=?]", blog_path
     assert_select "a[href=?]", about_path
-
-    get portfolio_path
-    assert_template 'static_pages/under_construction'
-    assert_select "h1", "Bison Hubert Portfolio Site"
 
     get about_path
     assert_template 'static_pages/about'
-    assert_select "a[href=?]", portfolio_path
-    assert_select "a[href=?]", blog_path
     assert_select "a[href=?]", root_path
   end
 end
