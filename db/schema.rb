6cf7b20642b5d598d0c963b69cb219a295e6578f
Bison Hubert <davidbisonhubert@gmail.com>
2017-06-21 17:56:40 -0700

    Add longer sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606005618) do
+ActiveRecord::Schema.define(version: 20170622002738) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170606005618) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
 end
