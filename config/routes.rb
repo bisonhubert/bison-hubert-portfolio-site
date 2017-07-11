e2076a2ca5efd8e69c8d37227f108351a99f9130
Bison Hubert <davidbisonhubert@gmail.com>
2017-07-11 10:53:50 -0700

    Organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get '/home',                to: 'static_pages#home'
-  get '/about',               to: 'static_pages#about'
-  get '/portfolio',           to: 'static_pages#portfolio'
-  get '/resume',              to: 'static_pages#resume'
-  get '/blog',                to: 'articles#index'
-  get '/under_construction',  to: 'static_pages#under_construction'
+  get    '/home',                 to: 'static_pages#home'
+  get    '/about',                to: 'static_pages#about'
+  get    '/portfolio',            to: 'static_pages#portfolio'
+  get    '/resume',               to: 'static_pages#resume'
+  get    '/blog',                 to: 'articles#index'
+  get    '/under_construction',   to: 'static_pages#under_construction'
 
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get    '/login',                to: 'sessions#new'
+  post   '/login',                to: 'sessions#create'
+  delete '/logout',               to: 'sessions#destroy'
   
   resources :articles, path: 'blog'
 end
