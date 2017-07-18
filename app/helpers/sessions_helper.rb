52d052abcc3857e18dc6f714036a4713d897fdfa
Bison Hubert <davidbisonhubert@gmail.com>
2017-07-17 22:21:49 -0700

    Add limitations to article and user actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,6 +25,12 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def logged_in_user
+    unless logged_in?
+       redirect_to root_path
+    end
+  end
+
   def log_out
     session.delete(:user_id)
     @current_user = nil
